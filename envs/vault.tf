8f35eafaec0dbcbfdd14bf37e40dc9411e3df71f
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-14 08:17:46 +0000

    :recycle: 認証局用非対称キー追加

--- envs/vault.tf
+++ envs/vault.tf
@@ -7,3 +7,24 @@ resource "oci_kms_vault" "default" {
   vault_type     = "DEFAULT"
   defined_tags   = local.common_defined_tags
 }
+
+/************************************************************
+KMS Key
+************************************************************/
+resource "oci_kms_key" "key_rsa" {
+  compartment_id      = oci_identity_compartment.workload.id
+  display_name        = "certificates-hsm-key"
+  desired_state       = "ENABLED"
+  # OCI Certificates 認証局用キーの保護モードは HSMのみ対応
+  # Software は不可
+  protection_mode     = "HSM"
+  management_endpoint = oci_kms_vault.default.management_endpoint
+  key_shape {
+    # OCI Certificates 認証局用キーのアルゴリズムは非対称キーのみ対応
+    # AES は不可。RSA / ECDSA 可。
+    algorithm = "RSA"
+    length    = 512
+  }
+  is_auto_rotation_enabled = false
+  defined_tags             = local.common_defined_tags
+}
