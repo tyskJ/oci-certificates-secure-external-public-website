ff56757affd6f727fcadea7e7ec663956387e8d2
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-15 21:16:37 +0900

    :recycle: サーバー証明書作成

--- envs/certificates.tf
+++ envs/certificates.tf
@@ -54,3 +54,56 @@ resource "oci_certificates_management_certificate_authority" "root_ca" {
   # }
   defined_tags = local.common_defined_tags
 }
+
+/************************************************************
+Certificate
+************************************************************/
+resource "oci_certificates_management_certificate" "certificate_flb" {
+  compartment_id = oci_identity_compartment.workload.id
+  name           = "flb-server-certificates"
+  description    = "For FLB Server Certificates"
+  certificate_config {
+    issuer_certificate_authority_id = oci_certificates_management_certificate_authority.root_ca.id
+    config_type                     = "ISSUED_BY_INTERNAL_CA"
+    subject {
+      common_name            = "www.${var.zone_name}"
+      country                = "JP"
+      state_or_province_name = "Tokyo"
+      # distinguished_name_qualifier = null
+      # domain_component             = null
+      # generation_qualifier         = null
+      # given_name                   = null
+      # initials                     = null
+      # locality_name                = null
+      # organization                 = null
+      # organizational_unit          = null
+      # pseudonym                    = null
+      # serial_number                = null
+      # street                       = null
+      # surname                      = null
+      # title                        = null
+      # user_id                      = null
+    }
+    subject_alternative_names {
+      type  = "DNS"
+      value = "www.${var.zone_name}"
+    }
+    certificate_profile_type = "TLS_SERVER"
+    key_algorithm            = "RSA2048"
+    signature_algorithm      = "SHA256_WITH_RSA"
+    # validity を指定すると「400-InvalidParameter」となる
+    # 指定しなければ、有効期限開始は作成時刻から、有効期限終了日はMAXになるのでOK
+    # validity {
+    #   # 有効期限開始日＋１日～2037年12月31日の範囲で指定可能
+    #   time_of_validity_not_after = "2037-12-31T23:59:59Z"
+    #   # 有効期限開始日を省略すると作成時刻から有効化される
+    #   time_of_validity_not_before = null
+    # }
+  }
+  certificate_rules {
+    rule_type              = "CERTIFICATE_RENEWAL_RULE"
+    renewal_interval       = "P90D"
+    advance_renewal_period = "P30D"
+  }
+  defined_tags = local.common_defined_tags
+}
