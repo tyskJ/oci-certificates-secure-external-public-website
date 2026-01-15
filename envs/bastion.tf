05f8240bc6e3b7312462eb18e7b5d5bd74a186cd
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-15 23:18:10 +0900

    :bug: Bastion Private Endpoint 配置先サブネット修正

--- envs/bastion.tf
+++ envs/bastion.tf
@@ -28,7 +28,7 @@ resource "oci_bastion_bastion" "this" {
   name                         = "bastion"
   compartment_id               = oci_identity_compartment.workload.id
   bastion_type                 = "STANDARD"
-  target_subnet_id             = oci_core_subnet.private_bastion.id
+  target_subnet_id             = oci_core_subnet.private.id
   dns_proxy_status             = "ENABLED" # Flag to enable FQDN and SOCKS5 Proxy Support.
   client_cidr_block_allow_list = [var.source_ip]
   max_session_ttl_in_seconds   = 10800 # Max minutes (3 hours)
