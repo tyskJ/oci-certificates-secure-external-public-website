42cdfeb06f6ba5cb41e77555677eb2594dabf812
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-16 14:08:45 +0900

    :recycle: FLB用レコード追加

--- envs/dns.tf
+++ envs/dns.tf
@@ -9,3 +9,18 @@ resource "oci_dns_zone" "public_zone" {
   dnssec_state   = "DISABLED"
   defined_tags   = local.common_defined_tags
 }
+
+/************************************************************
+Record - A
+************************************************************/
+resource "oci_dns_rrset" "rrset_a" {
+  zone_name_or_id = oci_dns_zone.public_zone.name
+  domain          = "www.${oci_dns_zone.public_zone.name}"
+  rtype           = "A"
+  items {
+    domain = "www.${oci_dns_zone.public_zone.name}"
+    rdata  = oci_load_balancer_load_balancer.flb.ip_address_details[0].ip_address
+    rtype  = "A"
+    ttl    = 60
+  }
+}
