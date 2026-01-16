d2cb83db4cdf9fc901098daae5940641282165ce
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-16 10:27:55 +0900

    :recycle: FLB 用 Reserved Public IP追加

---
+++ envs/elb.tf
@@ -0,0 +1,13 @@
+/************************************************************
+Reserved Public IPs
+************************************************************/
+resource "oci_core_public_ip" "flb" {
+  compartment_id = oci_identity_compartment.workload.id
+  lifetime       = "RESERVED"
+  display_name   = "flb-public-ip"
+  defined_tags   = local.common_defined_tags
+  # # IP Pool を指定しない場合は、Oracle管理のPublic IPから予約される
+  # public_ip_pool_id = null
+  # # Ephemeral Public IP の場合は必須
+  # private_ip_id     = null
+}
