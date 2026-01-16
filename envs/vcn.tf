a123cdfd2b5459eeb7f15f9817518ac76d3f0036
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-01-16 10:25:21 +0900

    :recycle: Reserved Public IP 追加

--- envs/vcn.tf
+++ envs/vcn.tf
@@ -96,6 +96,20 @@ resource "oci_core_internet_gateway" "igw" {
 }
 
 /************************************************************
+Reserved Public IPs
+************************************************************/
+resource "oci_core_public_ip" "ngw" {
+  compartment_id = oci_identity_compartment.workload.id
+  lifetime       = "RESERVED"
+  display_name   = "ngw-public-ip"
+  defined_tags   = local.common_defined_tags
+  # # IP Pool を指定しない場合は、Oracle管理のPublic IPから予約される
+  # public_ip_pool_id = null
+  # # Ephemeral Public IP の場合は必須
+  # private_ip_id     = null
+}
+
+/************************************************************
 NAT Gateway
 ************************************************************/
 resource "oci_core_nat_gateway" "ngw" {
@@ -103,6 +117,7 @@ resource "oci_core_nat_gateway" "ngw" {
   display_name   = "ngw"
   vcn_id         = oci_core_vcn.vcn.id
   block_traffic  = false
+  public_ip_id   = oci_core_public_ip.ngw.id
   defined_tags   = local.common_defined_tags
 }
 
